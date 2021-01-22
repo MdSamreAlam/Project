8a8ce0df42be4a65c46e3f1a74b26caa33a913ee
MdSamreAlam <mohammedsamre.alam@augmentolabs.com>
2021-01-22 14:30:01 +0530

    Added Calculator Method and Unit Test
    Added there method with name Add,Subtract and Multiply and also Implemented Unite test logic

---
+++ Calculator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Calculator
+{
+   public class Calculator
+    {
+        public int Add(int number1,int number2)
+        {
+            var result= number1 + number2;
+            return result;
+        }
+        public int Subtract(int number1, int number2)
+        {
+            var result = number1 - number2;
+            return result;
+        }
+        public int Mul(int number1, int number2)
+        {
+            var result = number1 * number2;
+            return result;
+        }
+    }
+}
---
+++ CalculatorTest.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Calculator.Test
+{
+   
+    public  class CalculatorTest
+    {
+        Calculator calculator = new Calculator();
+        [Fact]
+        public void ShouldAddTwoNumber()
+        {
+            //Arrange
+            int number1 = 10;
+            int number2 = 15;
+            int expected = 25;
+            //Act 
+           
+            int actual = calculator.Add(number1, number2);
+
+            //Assert
+            Assert.Equal(expected, actual);
+        }
+        [Fact]
+        public void ShouldSubtract()
+        {
+            // Arranage
+            int number1 = 20;
+            int number2 = 15;
+            int expected = 5;
+            //Act
+            int actual = calculator.Subtract(number1, number2);
+
+            //Assert
+            Assert.Equal(expected, actual);
+        }
+        [Fact]
+        public void ShouldMulTwoNumber()
+        {
+            //Arrange
+            int number1 = 5;
+            int number2 = 4;
+            int expected = 20;
+            //Act
+          int actual=  calculator.Mul(number1, number2);
+            //Assert
+            Assert.Equal(expected, actual);
+        }
+
+    }
+}
